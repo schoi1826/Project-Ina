e3be5cae7ae6c7042cc36b282cfd43122faece56
snyshadham12 <sn5hnj@virginia.edu>
2020-02-15 13:13:46 -0500

    Glossary list and Stories cards added without PDF

--- components/GlossaryScreen.js
+++ components/GlossaryScreen.js
@@ -1,12 +1,109 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, FlatList, StyleSheet } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 export default class GlossaryScreen extends Component {
 	render() {
 		return (
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-		      <Text>Glossary Screen</Text>
+			<View style={{flex: 1, paddingBottom: 20}}>
+				<FlatList
+			      data = {DATA}
+			      renderItem={({item}) => <GlossaryItem name={item.name}/>}
+			    />
 		    </View>
 		);
 	}
 }
+
+function GlossaryItem({name}) {
+  return (
+    <View style={{paddingTop: 18, paddingLeft: 7, height: 60, borderWidth: 1, borderColor: '#e1e1e1', backgroundColor: '#ffffff',
+    flexDirection: 'row', justifyContent: 'flex-start'}}>
+    	<Icon name='chevron-right' color='steelblue'/>
+    	<Text style={{fontSize: 17, textAlign: 'right'}} onPress={() => {alert('Clicked ' + name);}}>{name}</Text>
+    </View>
+  );
+}
+
+const DATA = [
+  	{
+  		name: 'Alcohol During Pregnancy',
+  		id: 'alcoholDuringPregnancy',
+  	},
+  	{
+  		name: 'Breastfeeding',
+  		id: 'breastfeeding',
+  	},
+  	{
+  		name: 'Complicated Pregnancies',
+  		id: 'complicatedPregnancies',
+  	},
+  	{
+  		name: 'Domestic Violence',
+  		id: 'domesticViolence',
+  	},
+  	{
+  		name: 'Family Planning',
+  		id: 'familyPlanning',
+  	},
+  	{
+  		name: 'Gestational Diabetes',
+  		id: 'gestationalDiabetes',
+  	},
+  	{
+  		name: 'Healthy Diet',
+  		id: 'healthyDIet',
+  	},
+  	{
+  		name: 'Healthy Relationships',
+  		id: 'healthyRelationships',
+  	},
+  	{
+  		name: 'Medications During Pregnancy',
+  		id: 'medicationsDuringPregnancy',
+  	},
+  	{
+  		name: 'Next Steps After Learning You\'re Pregnant',
+  		id: 'nextSteps',
+  	},
+  	{
+  		name: 'Postpartum Depression',
+  		id: 'postpartumDepression',
+  	},
+  	{
+  		name: 'Pre-Conception Health',
+  		id: 'preConceptionHealth',
+  	},
+  	{
+  		name: 'Pre-eclampsia',
+  		id: 'preeclampsia',
+  	},
+  	{
+  		name: 'Prenatal Check-ups',
+  		id: 'prenatalCheckups',
+  	},
+  	{
+  		name: 'Prenatal Tests',
+  		id: 'prenatalTests',
+  	},
+  	{
+  		name: 'Preparing for Delivery',
+  		id: 'preparingForDelivery',
+  	},
+  	{
+  		name: 'Preterm Birth',
+  		id: 'pretermBirth',
+  	},
+  	{
+  		name: 'Substance Abuse',
+  		id: 'substaceAbuse',
+  	},
+  	{
+  		name: 'Teenage Pregnancy',
+  		id: 'teenagePregnancy',
+  	},
+  	{
+  		name: 'Ultrasound Exam',
+  		id: 'ultrasoundExam',
+  	},
+];
--- components/StoriesScreen.js
+++ components/StoriesScreen.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, FlatList, Image } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 
 export default class StoriesScreen extends Component {
 	render() {
 		return (
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-		      <Text>Stories Screen</Text>
+			<View style={{flex: 1, paddingBottom: 20}}>
+				<FlatList
+			      data = {DATA}
+			      renderItem={({item}) => <StoryItem name={item.name} imagePath={item.imagePath}/>}
+			    />
 		    </View>
 		);
 	}
 }
+
+function StoryItem({name, imagePath}) {
+  return (
+    <View style={{flex: 1}}>
+		<Card image = {imagePath}>
+			<Text style={{fontSize: 17, fontWeight: 'bold'}}>{name}</Text>
+		</Card>
+	</View>
+  );
+}
+
+const DATA = [
+	{
+		name: 'Dakota Pregnancy Traditions',
+		imagePath: require("./pregnancy_traditions.png"),
+	},
+	{
+		name: 'Pregnancy as Sacred',
+		imagePath: require("./pregnancy_as_sacred.jpg"),
+	},
+	{
+		name: 'The Woman in the Sky',
+		imagePath: require("./the_woman_in_the_sky.jpg"),
+	},
+]
